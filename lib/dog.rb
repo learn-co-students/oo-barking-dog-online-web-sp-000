ef6e35c9d2ff1ba78feb1dbb4fc1d0117d928df7
Sharon A <sharon.alajajian@gmail.com>
2018-10-01 11:42:53 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Dog
+    #writer/setter method. give dog a name.
+    def name=(myname)
+        @name = myname
+    end
+    
+    #reader/getter method. return instance name.
+    def name
+        @name
+    end
+    
+    #puts 'woof!' when called on an instance of dog.
+    def bark
+       puts "woof!"
+    end
+end
