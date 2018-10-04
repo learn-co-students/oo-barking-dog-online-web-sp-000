b7a148e9588bb8bfd8297888a22623b129026dab
Sam Tamburri <tamburrisam@gmail.com>
2018-10-04 00:09:53 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Dog 
+  
+   def name=(dogs_name, dogs_bark = "woof!")
+    @name = dogs_name
+    @bark = dogs_bark
+  end
+  
+  def name 
+    @name 
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
+  
+ 
+
+
+  
