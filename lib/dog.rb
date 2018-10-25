8c0e96e024a55242915cb82f21b98e38bb129298
Benjamin M Lancaster <bmlancaster07@gmail.com>
2018-10-25 20:02:52 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Dog
+  def name=(dogs_name) #setter
+    @name = dogs_name
+  end 
+
+  def name            #getter
+    @name
+  end 
+  
+  def bark            #bark method
+    puts "woof!"
+  end
+  
+end
+    
