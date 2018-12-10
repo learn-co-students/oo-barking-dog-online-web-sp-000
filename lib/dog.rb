0b005febcfea3f34c156e3bba7e8750d183da031
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-10 00:32:58 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Dog
+  def initialize(dogs_name)
+    @name=dogs_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name=new_name
+  end
+  
+  def bark
+    puts "Woof!"
+  end
+end
+
+fido= Dog.new
+
+fido.name
+
+fido.name= "Fido"
