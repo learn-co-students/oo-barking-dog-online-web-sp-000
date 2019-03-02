37beb409c1be7ec23c8bfd16f5926c5564408e2e
Donna Amos <donnacamos.developer@gmail.com>
2019-03-02 17:44:01 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Dog 
+  def name=(fido)
+    @name = fido
+  end
+  def name
+    @name
+  end
+  def bark=(woof!)
+    @bark = woof!
+  end
+  def bark
+    @bark
+  end 
+end
