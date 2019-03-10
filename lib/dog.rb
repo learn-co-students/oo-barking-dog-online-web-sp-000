ce78b9253c86e16b7164938f3ba6d50f7cf2fe77
Heeraj Ramsanai <ramsanai_heeraj@live.com>
2019-03-10 01:48:09 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+# Your code goes here
+class Dog
+  def intialize(dogs_name)
+    @name=dogs_name
+  end
+
+  def name=(dogs_name)
+    @name=dogs_name
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
