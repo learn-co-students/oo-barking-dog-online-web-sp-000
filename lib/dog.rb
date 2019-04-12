bf24a7a48105f3ea0d958d30819acd937e7151cb
Nicole Rothenay <nicoler@iccreativeagency.com>
2019-04-12 18:56:31 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+class Dog
+  def initialize
+    @name = name
+
+  end
+  
+  #setter methods
+  def name=(name)
+    @name = name
+  end
+  def bark=(bark)
+    @bark = bark
+    puts "woof!"
+  end
+  
+  #getter methods
+  def name
+    @name
+  end
+  
+  def bark
+    @bark
+    puts "woof!"
+  end
+end
