96dca01061be86a6d98d9f8133445379603a2efb
Connor Cowan <ccowan003@comcast.net>
2019-06-19 21:32:23 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Dog
+  def name=(dog_name)             #setter method
+    @this_dogs_name = dog_name
+  end
+
+  def name                        #getter method
+    @this_dogs_name
+  end
+
+  def bark                      #barking method
+    puts "Woof!"
+  end
+
+
+fido = Dog.new
+puts fido.bark
+#fido.name = "Fido"
+
+fido.bark
