5c470d33e6f90b312d2ab28831829e673b8a2dcc
Christopher Dent <christopherdent01@gmail.com>
2019-08-09 02:23:49 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+
+
+class Dog
+  def name=(dogname)
+    @a_dogs_name = dogname 
+  end 
+  
+  def name
+    @a_dogs_name
+  end 
+  
+  def bark
+    puts "woof!"
+  end 
+  
+  
+end 
