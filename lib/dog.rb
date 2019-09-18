a5bd6aacdbabb50d96acbc65a4aa1fe9828c78e3
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-09-18 22:56:26 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Dog
+  #def initiate(dog_name) can be written like this, however the test is explicitly looking for #name=
+  def name= (dog_name)
+    @dog_name = dog_name
+  end
+  
+  def name
+    @dog_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
+
+
+
+samantha = Dog.new
+samantha.name= ("Sammy")
+
+samantha.bark
+puts samantha.name
