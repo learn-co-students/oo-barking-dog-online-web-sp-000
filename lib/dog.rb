4f5242d559c309921317c80d369b9ed2f8f2093d
Kate Summerill <kaitlynsummerill@gmail.com>
2019-12-17 23:50:08 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog
+  # write/setter method
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+  # read/getter method
+  def name
+    @this_dogs_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+end
