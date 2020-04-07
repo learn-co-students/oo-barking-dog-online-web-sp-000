8b53231ef284751eb524ed714ddba5c6e2e49a6d
Adam Spencer <aspencer0919@gmail.com>
2020-04-07 16:27:11 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Dog
+  def name=(name)
+    @dog_name = name
+  end
+
+  def name
+    @dog_name
+  end
+
+  def bark=(woof)
+    @bark = woof
+  end
+
+  def bark
+    @bark
+    puts "woof!"
+  end
+end
