be48e414dda51b4a583e7ed871effdad583687d1
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-15 21:01:08 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Dog 
+  
+  Dog.new
+  
+  def name=(x)
+    @dogsname = x
+  end
+  
+  def name 
+    @dogsname
+  end
+  
+  def bark 
+    puts "woof!"
+  end
+
+end
+    
