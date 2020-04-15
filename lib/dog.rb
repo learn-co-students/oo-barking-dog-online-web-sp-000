aeb4dad219150954a4ae0fa0b4cec1e97c978106
Santiago Molinari <santomolinari@gmail.com>
2020-04-15 16:23:14 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog 
+  
+  def name=(dogs_name)
+    @nombre_perro = dogs_name
+  end
+  
+  
+  def name 
+    @nombre_perro
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
