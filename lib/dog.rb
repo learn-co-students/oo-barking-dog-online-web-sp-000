7f698116f82a529de1d497f62c446cdbac4be3a9
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-13 21:40:46 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,15 @@
-# Your code goes here!
 class Dog
-  def name=(dog_name)
-    @dogs_name = dog_name
+
+  def name=(name)
+    @name = name
   end
 
   def name
-    @dogs_name
+    @name
   end
 
   def bark
     puts "woof!"
   end
+
 end
