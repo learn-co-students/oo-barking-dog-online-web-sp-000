c8c8d1947b7ce02f51a93dce788e26f618303699
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-02 18:31:53 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Dog
+
+  def name=(given_name)
+    @new_given_name = given_name
+  end
+
+  def name
+    @new_given_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+  
+end
