ba90df561a5cdf5c1fdcd15f733f6c8e40842866
Christerle Nounoute <christerlesn@gmail.com>
2020-09-23 19:57:14 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+class Dog
+def name=(dog_name)
+  @the_dog_name = dog_name
+end
+
+def name
+  @the_dog_name
+end
+
+#binding.pry
+
+def bark
+  puts "woof!"
+end
+end
