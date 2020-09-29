e6ffe1e0cce6dc5b2e03a9d31c1e487984ae3745
Alla Kuzminova <alchik@gmail.com>
2020-09-29 00:46:45 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Dog
+  def name=(dog_name)
+    @dog_name_instace = dog_name
+  end
+
+  def name
+    @dog_name_instace
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
