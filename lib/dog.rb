411308ba7e3e59263d4f9d6ab3c3c8c5d860055e
Alison Williams <apw5011@gmail.com>
2020-11-18 00:10:21 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog 
+  
+  def name=(dog_name)
+    @new_dogs_name = dog_name
+  end 
+  
+  def name 
+    @new_dogs_name
+  end
+  
+  def bark 
+    puts "woof!"
+  end
+  
+end 
