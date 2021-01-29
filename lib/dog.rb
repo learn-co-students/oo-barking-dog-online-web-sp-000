f83f3fae21a93b85ea4019caa428adece00e3ec4
Elliott Vargo <vargoelliott@gmail.com>
2021-01-29 02:33:48 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Dog
+def name=(input_name)
+  @dog_name = input_name
+end
+
+def name
+  @dog_name
+end
+
+def bark
+puts "woof!"
+end
+
+end
+
+
+doggo = Dog.new
+
+doggo.bark
