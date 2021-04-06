bbeecca7b173cf2d0dd99862342818aafd27fb0e
Harrison Price <hprice518@gmail.com>
2021-04-06 19:36:59 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+
+class Dog 
+  def name= (this_dogs_name) 
+    name
+  end
+  
+  def name 
+    @dogs_name
+  end
+  
+  def bark= (bark)
+    bark
+  end
+  
+  def bark
+    puts "Woof!"
+  end
+  
+  
+end
