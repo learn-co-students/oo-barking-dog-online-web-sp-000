a936e4c3be7b0642a1b6ac5d807cc47f330350ff
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-12 21:05:20 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Dog 
+  def bark
+    puts "woof!"
+  end 
+  
+  def name=(dog_name)
+    @dogs_name = dog_name
+  end
+  
+  def name
+    @dogs_name
+  end 
+  
+end
