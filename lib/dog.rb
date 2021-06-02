c21589e44c190c849c1ef84d09e6b457c21b7987
DJ Taylor <dtaylor88@gmail.com>
2021-06-02 22:35:59 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Dog
+  
+  #setter method 
+  def name=(dog_name)
+    @this_dogs_name = dog_name 
+  end 
+  
+  #getter method
+  def name 
+    @this_dogs_name 
+  end
+  
+  #instance method 
+  def bark
+    puts "woof!"
+  end 
+  
+end
+
+  
