10a698af29915a436936a86007502bee97aedfd3
James Tillman <james@itme-music.com>
2021-06-06 20:55:09 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Dog
+
+  def name=(this_dogs_name)
+    @this_dogs_name = this_dogs_name
+  end
+
+  def name
+    @this_dogs_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+
+end
